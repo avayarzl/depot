e90337f8996c72e0aa82affe7723c165b05720d7
Avaya <avaya.rijal@gmail.com>
2014-03-30 11:59:49 +0545

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '1786187d40818e92f178a6236456cc75ed40e9cc85a6183d7635ef9ccfa4f42710a1c1c4de31f2654ea72d5402a4c22542f58d5f0cbe2870dfb604fa6b321abc'
